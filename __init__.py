7d660e6a638b928177d6e1d852a9b229a0e5e0d5
chsponciano <chs.ponciano@gmail.com>
2020-05-31 15:12:47 -0300

    modify

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ import boto3
 
 # Comprehend constant
 region = 'us-east-2'
-aws_access_key_id = 'AKIAJJB2PO54FEPYHUZQ'
-aws_secret_access_key = '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'
+aws_access_key_id = '=== aws_access_key_id ==='
+aws_secret_access_key = '=== aws_secret_access_key ==='
 
 if __name__ == '__main__':
     _text = 'Amazon Comprehend is a natural language processing (NLP) service that uses machine learning to find insights and relationships in text.'
