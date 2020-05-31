924489d2685332d589ff2a9661dbd1b82a6a16d9
chsponciano <chs.ponciano@gmail.com>
2020-05-31 14:56:48 -0300

    modify

--- __init__.py
+++ __init__.py
@@ -1,28 +1,17 @@
-import neural_network
-import functions
-import numpy as np
 
+import boto3
+import csv
+import json
 
-def training():
-    _train_data, _train_labels, _test_data, _test_labels = functions.load_trainig_data()
-    print("Training entries: {}, labels: {}".format(len(_train_data), len(_train_labels)))
-    _word_index = functions.get_word_settings()
-    _train_data = functions.format_sequence_elements(_train_data, _word_index)
-    _test_data = functions.format_sequence_elements(_test_data, _word_index)
-    print(len(_train_data[0]), len(_train_data[1]))
+# Comprehend constant
+region = 'us-east-2'
+aws_access_key_id = 'AKIAJJB2PO54FEPYHUZQ'
+aws_secret_access_key = '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'
 
-    _train_x, _train_y, _test_x, _test_y = functions.splitting(_train_data, _train_labels)
-
-    _model = neural_network.build(functions.get_num_values())
-    _model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    _model.fit(_train_x, _train_y, epochs=40, batch_size=512, validation_data=(_test_x, _test_y), verbose=1)
-    _model.evaluate(_test_data, _test_labels)
-    _model.save(f'models/model')
-
-def prodution():
-    _model = neural_network.load()
-    print(_model.predict(np.array(['Brasil', 'Dinheiro'])))
-
-if __name__ == "__main__":
-    # training()
-    prodution()
+if __name__ == '__main__':
+    _text = 'Amazon Comprehend is a natural language processing (NLP) service that uses machine learning to find insights and relationships in text.'
+    _comprehend = boto3.client('comprehend', region_name=region, aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id)
+    _language_code = _comprehend.detect_dominant_language(Text=_text)['Languages'][0]['LanguageCode']
+    print(_comprehend.detect_entities(Text=_text, LanguageCode=_language_code)) # detecting named entities
+    print(_comprehend.detect_key_phrases(Text=_text, LanguageCode=_language_code)) # detecting key phrases
+    print(_comprehend.detect_sentiment(Text=_text, LanguageCode=_language_code)) # detecting sentiment
--- setting.py
+++
@@ -1,7 +0,0 @@
-# Author: Carlos Henrique Ponciano da Silva
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-def get_env(env):
-    return os.getenv(env)
